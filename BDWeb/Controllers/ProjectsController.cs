ebe9d11dac4ac66adb48ce6813e561206f51e863
Bartlomiej Nowak <bartlomiej.nowak22@gmail.com>
2020-08-05 19:10:20 +0200

    Add Two Controllers (Home and Projects) and Two Views for Home Methods.
    Adds two controllers each with two methods and two views for home methods. For now controllers are empty and views doesn't yet have look.

---
+++ BDWeb/Controllers/ProjectsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDWeb.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public string GetAllProjectsCategories()
+        {
+            return "all projects categories";
+        }
+
+        public string GetAllMachineLearningProjects()
+        {
+            return "machine learning projescts";
+        }
+    }
+}
